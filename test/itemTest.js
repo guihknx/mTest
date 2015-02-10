c62e0ccaf62076e53a6b5be9a727b375b276bceb
Guilherme Henrique <guih@hotmail.com.br>
2015-02-10 14:46:37 -0200

    Add new test

--- test/itemTest.js
+++ test/itemTest.js
@@ -1,15 +1,28 @@
 var expect = require("chai").expect;
-var items = require("../lib/items.js");
- 
+var items = require("../lib/items.js").Items;
+var assertions = require('mocha').it
+var assert = require('assert');
+
 describe("Items", function(){
-    describe("#returnValid()", function(){
-       it("should return valid item value", function(){
-       	
-           var args = ["--valueDigit=0001", "--valueStr=Test"];
-           var results = items.returnValid(args);
- 
-           expect(results).to.have.a.property("valueDigit", 001);
-           expect(results).to.have.a.property("valueStr", "Test");
-       });
-   });
+	describe("#returnValid()", function(){
+		it("should return valid item value", function(){
+			
+			var results = items.returnValid();
+
+			expect(results).to.have.a.property("valueDigit", 001);
+			expect(results).to.have.a.property("valueStr", "Test");
+		});
+	});
+
+	describe("#returnValidByParams", function(){
+		it("should return valid item value based in params", function(){
+			
+			var param = 5;
+
+			var result = items.returnValidByParams(param);
+
+			expect(result).to.be.equal(5);
+		});
+	});
+	
 });
